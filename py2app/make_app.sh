4c2386e9745c68e48cee4cb1fbf88be0e8569c34
John Bender <john@generalui.com>
2017-03-18 09:42:09 -0700

    build 0.5.16 for Mac

--- py2app/make_app.sh
+++ py2app/make_app.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 echo "############### building ###############"
 rm -rf build
 sudo rm -rf dist
 rm -rf Ctrax/*
-cp -R ../Ctrax/* Ctrax/
+cp -R ../Ctrax/ Ctrax/
 cp Ctrax-script.py Ctrax/
 for subcomp in 'kcluster2d' 'houghcircles' 'hungarian' 'tracking_funcs' 'psutil'
 do
@@ -12,7 +12,8 @@ do
    cp -R ../$subcomp .
 done
 
-python setup_py2app.py py2app && \
-  cp -R ../Ctrax/xrc dist/Ctrax.app/Contents/Resources/lib/python2.7 && \
-  cp mac-xrc/* dist/Ctrax.app/Contents/Resources/lib/python2.7/xrc && \
-  cp -R ../Ctrax/icons dist/Ctrax.app/Contents/Resources/lib/python2.7
+python setup_py2app.py py2app
+
+cp -R ../Ctrax/xrc dist/Ctrax.app/Contents/Resources/lib/python2.7
+cp mac-xrc/* dist/Ctrax.app/Contents/Resources/lib/python2.7/xrc
+cp -R ../Ctrax/icons dist/Ctrax.app/Contents/Resources/lib/python2.7
